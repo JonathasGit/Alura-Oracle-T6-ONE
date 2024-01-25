d9f8114548617d4b4bb024568a2983e278c609df
JonathasGit <jonathas-25@hotmail.com>
2024-01-24 23:05:51 -0300

    acerto e erro

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,4 +1,12 @@
 alert("Boas vindas ao jogo do número Secreto");
 let numeroSecreto = 29; 
 
-prompt('Escolha um número entre 1 e 30');
+let chute = prompt('Escolha um número entre 1 e 30');
+
+//condição de comparar
+if(chute == numeroSecreto){
+     console.log('Você acertou')
+    //alert('Você acertou')
+} else{
+    console.log('Errou')
+}
